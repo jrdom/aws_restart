8a6396301e4366560ac3bb2af0be08fcffd69e62
Junior Domingos <99135620+jrdom@users.noreply.github.com>
2023-10-09 21:06:20 -0300

    Add files via upload

---
+++ LAB109.py
@@ -0,0 +1,11 @@
+inteiro=123456
+
+print(inteiro)
+
+var="responde o professor"
+
+print(var)
+
+decimal=1.45
+print(decimal)
+
---
+++ lab112.py
@@ -0,0 +1,4 @@
+myMixedTypeList = [45, 290578, 1.02, True, "My dog is on the bed.", "45"]
+for receba in myMixedTypeList:
+    print("{} qual é o tipo de dado {}".format(receba,type(receba)))
+    
